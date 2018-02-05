f8db2458c037066dfb3db8f7086a23976ff88a74
alexclp <alexandru.clapa@gmail.com>
2018-02-05 16:35:31 +0000

    Getting measurements for MAC

--- Sources/App/Models/PositionCalculator.swift
+++ Sources/App/Models/PositionCalculator.swift
@@ -3,10 +3,52 @@ import Foundation
 
 final class PositionCalculator: NSObject {
     static let shared = PositionCalculator()
+    private let baseURLAPI = "https://wifi-nav-api.herokuapp.com"
 
 	private override init() { }
 
-    func determinePosition(for data: Data) {
+    func determinePosition(for measurementsCollection: MeasurementsJSONCollection) {
+        for measurement in measurementsCollection.measurements {
+            let currentScanMac = measurement.macAddress
+            print(currentScanMac)
+            searchForOldMeasurements(for: currentScanMac, completion: { (success, oldMeasurements) in
+                if success == true {
+                    guard let m = oldMeasurements else { print("Nothing found"); return }
+                    print(m)
+                }
+            })
+        }
+    }
 
+    private func searchForOldMeasurements(for macAddress: String, completion: @escaping (Bool, [MeasurementElement]?) -> Void) {
+        var toReturn = [MeasurementElement]()
+        HTTPClient.shared.request(urlString: baseURLAPI + "/measurements/address/\(macAddress)", method: "GET", parameters: nil) { (success, data) in
+            if success == true {
+                do {
+                    print("Succesfully got the measurements for address \(macAddress)")
+                    guard let data = data else { 
+                        completion(false, nil)
+                        return 
+                    }
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
+                        print(json)
+                        for network in json {
+                            if let signal = network["signalStrength"] as? Int {
+                                toReturn.append(MeasurementElement(signalStrength: signal, name: "Current", macAddress: macAddress))
+                            }  
+                        }
+                    } else {
+                        completion(false, nil)
+                    }
+                    completion(false, nil)
+                } catch {
+					print(error.localizedDescription)
+                    completion(false, nil)
+				} 
+            }  else {
+                print("Failed to get the measurements for address \(macAddress)")
+                completion(false, nil)
+            }
+        }
     }
 }
--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,22 +1,28 @@
 import Vapor
 import Foundation
 
+struct MeasurementElement: Decodable {
+    let signalStrength: Int
+    let name: String
+    let macAddress: String
 
-
-struct MeasurementsJSONArray: Decodable {
-    struct MeasurementElement: Decodable {
-        let signalStrength: Int
-        let name: String
-        let macAddress: String
+    init(signalStrength: Int, name: String, macAddress: String) {
+        self.signalStrength = signalStrength
+        self.name = name
+        self.macAddress = macAddress
     }
+}
+
+struct MeasurementsJSONCollection: Decodable {
     let measurements: [MeasurementElement]
 }
 
 extension Droplet {
-    
     func setupRoutes() throws {
         post("determinePosition") { req in 
-            print(try req.decodeJSONBody(MeasurementsJSONArray.self))
+            print(try req.decodeJSONBody(MeasurementsJSONCollection.self))
+            let measurementsCollection = try req.decodeJSONBody(MeasurementsJSONCollection.self)
+            PositionCalculator.shared.determinePosition(for: measurementsCollection)
             return "Response"
         }
     }
